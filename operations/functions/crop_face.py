a8725d98ad8f05d21521ef61b69cf0f7aa17a965
Marek <zylamarek@gmail.com>
2019-06-12 18:03:02 +0200

    added margin param to CropFace

--- operations/functions/crop_face.py
+++ operations/functions/crop_face.py
@@ -8,9 +8,16 @@ class CropFaceException(Exception):
 
 
 class CropFace(Function):
+    def __init__(self, margin=0, *args, **kwargs):
+        self.margin = int(margin)
+        super(CropFace, self).__init__(*args, **kwargs)
+
     def apply_single(self, img):
         boxes = face_recognition.face_locations(img, model='hog')
         if not boxes:
             raise CropFaceException('No face detected')
         box = boxes[0]
-        return img[box[0]:box[2] + 1, box[3]:box[1] + 1]
+        return img[max(0, box[0] - self.margin):box[2] + 1 + self.margin, max(0, box[3] - self.margin):box[1] + 1 + self.margin]
+
+    def name(self):
+        return super(CropFace, self).name() + ('_%d' % self.margin)
