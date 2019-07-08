51880243890cf0083ae4fb3b1524601bc887f4a8
Marek <zylamarek@gmail.com>
2019-07-08 19:54:47 +0200

    added Kalman filter to AlignFace for smooth sequential input

--- operations/functions/align_face.py
+++ operations/functions/align_face.py
@@ -1,9 +1,9 @@
-from imutils.face_utils import FaceAligner
 from PIL import Image
 import numpy as np
+from pykalman import KalmanFilter
 
 from .function import Function
-from utils import predictor_setup
+from utils import predictor_setup, FaceAlignerSplit
 
 
 class AlignFaceException(Exception):
@@ -18,20 +18,49 @@ class AlignFace(Function):
         self.desired_face_height = int(desired_face_height)
 
         self.detector, self.predictor = predictor_setup()
-        self.face_aligner = FaceAligner(self.predictor, desiredLeftEye=(0.3, 0.35))
+        self.face_aligner = FaceAlignerSplit(self.predictor, desiredLeftEye=(0.32, 0.3),
+                                             desiredFaceWidth=self.desired_face_width,
+                                             desiredFaceHeight=self.desired_face_height)
         super(AlignFace, self).__init__(do_analysis=True, *args, **kwargs)
+
+    def fix_meta_sequence(self, metas):
+        measurements = np.ma.asarray(np.zeros((len(metas), 6)))
+        for i, meta in enumerate(metas):
+            if meta is not None:
+                measurements[i] = meta.flatten()
+            else:
+                measurements[i] = np.ma.masked
+
+        means = np.mean(measurements, axis=0)
+        vars = np.var(measurements, axis=0)
+        measurements -= means
+        measurements /= vars
+
+        kf = KalmanFilter(n_dim_obs=6, n_dim_state=6,
+                          observation_covariance=1e4 * np.eye(6),
+                          transition_covariance=1e4 * np.eye(6),
+                          em_vars=['initial_state_mean', 'initial_state_covariance'])
+        kf = kf.em(measurements)
+
+        smoothed, _ = kf.smooth(measurements)
+        smoothed *= vars
+        smoothed += means
+        metas = [np.reshape(sm, (2, 3)) for sm in smoothed]
+
+        return metas
 
     def analyze_single(self, img):
         boxes = self.detector(img)
         if not boxes:
             raise AlignFaceException('No face detected')
-        return boxes[0]
+        gray = np.asarray(Image.fromarray(img, mode='RGB').convert('L'))
+        M = self.face_aligner.analyze(gray, boxes[0])
+        return M
 
     def apply_single(self, img, meta=None):
         if meta is None:
             raise AlignFaceException('No face detected')
-        gray = np.asarray(Image.fromarray(img, mode='RGB').convert('L'))
-        return self.face_aligner.align(img, gray, meta)
+        return self.face_aligner.apply(img, meta)
 
     def name(self):
         return super(AlignFace, self).name() + ('_%dx%d' % (self.desired_face_width, self.desired_face_height))
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,9 @@
+from utils.face_aligner_split import FaceAlignerSplit
 from utils.interpret import interpret_op
 from utils.align_face_setup import predictor_setup
 
 __all__ = [
+    'FaceAlignerSplit',
     'interpret_op',
     'predictor_setup'
 ]
